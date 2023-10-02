d08b851c9f1fb584d96f62ea006bcc1844f5c71e
Nureini <nureinimoobe@gmail.com>
2023-10-02 18:29:48 +0100

    updated commit

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,56 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void MovementScript_Start_mB5D6534BCBCFA6728B7EB4283A1382CB20704F98 (void);
+extern void MovementScript_Update_mC379BABFFEDE2CBD654EB241D465000FE1F6B13B (void);
+extern void MovementScript__ctor_m2DD1B34C7EF8C0E7F06CD27B5AF9CEB9196A8FBA (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	MovementScript_Start_mB5D6534BCBCFA6728B7EB4283A1382CB20704F98,
+	MovementScript_Update_mC379BABFFEDE2CBD654EB241D465000FE1F6B13B,
+	MovementScript__ctor_m2DD1B34C7EF8C0E7F06CD27B5AF9CEB9196A8FBA,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	1098,
+	1098,
+	1098,
+	2120,
+	1098,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
